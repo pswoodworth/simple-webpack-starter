70ba3ef1996d3c3366afdfe400f2cec669ff05cb
Parker <pwoodworth@cb-m-atelio.ideocolab.com>
2017-06-29 18:38:36 -0400

    Added scanning to bleacon

--- index.js
+++ index.js
@@ -45,6 +45,7 @@ var minor = 0; // 0 - 65535
 var measuredPower = -59; // -128 - 127 (measured RSSI at 1 meter)
 
 Bleacon.startAdvertising(uuid, major, minor, measuredPower);
+Bleacon.startScanning();
 
 Bleacon.on('discover', function(bleacon) {
   console.log('discovered', bleacon);
