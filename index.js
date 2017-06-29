b1e6ab08854e79aece7c5ca37c565b7ccfbab54b
Parker <pswoodworth@gmail.com>
2017-06-29 17:29:04 -0400

    initial

---
+++ index.js
@@ -0,0 +1,38 @@
+var noble = require('noble');
+var bleno = require('bleno');
+
+noble.on('stateChange', function(state) {
+  if (state === 'poweredOn') {
+    noble.startScanning();
+  } else {
+    noble.stopScanning();
+  }
+});
+
+/*
+noble.on('discover', function(peripheral) {
+  peripheral.connect(function(error) {
+    console.log('connected to peripheral: ' + peripheral.uuid);
+    peripheral.discoverAllServicesAndCharacteristics((error, services, characteristics)=>{
+		console.log('discovered', services, 'with characteristics', characteristics);
+	});
+  });
+});
+
+noble.on('stateChange', function(state) {
+  if (state === 'poweredOn') {
+    noble.startScanning();
+  } else {
+    noble.stopScanning();
+  }
+});
+*/
+
+bleno.on('stateChange', function(state) {
+	var advertisementData = new Buffer("I AM THE TEST DATA");
+  if (state === 'poweredOn') {
+    bleno.startAdvertisingWithEIRData(advertisementData);
+  }
+});
+
+
