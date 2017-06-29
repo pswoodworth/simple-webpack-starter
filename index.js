897f5abeb30d41edf287dfe8d76456fba2d9ab43
Parker <pwoodworth@cb-m-atelio.ideocolab.com>
2017-06-29 18:27:19 -0400

    switched to bleacon

--- index.js
+++ index.js
@@ -1,37 +1,51 @@
-var noble = require('noble');
-var bleno = require('bleno');
+// var noble = require('noble');
+// var bleno = require('bleno');
+//
+// noble.on('stateChange', function(state) {
+//   if (state === 'poweredOn') {
+//     noble.startScanning();
+//   } else {
+//     noble.stopScanning();
+//   }
+// });
+//
+//
+// noble.on('discover', function(peripheral) {
+//   console.log('found', peripheral);
+//   // peripheral.connect(function(error) {
+//   //   console.log('connected to peripheral: ' + peripheral.uuid);
+//   //   peripheral.discoverAllServicesAndCharacteristics((error, services, characteristics)=>{
+// 	// 	console.log('discovered', services, 'with characteristics', characteristics);
+// 	// });
+//   // });
+// });
+//
+// noble.on('stateChange', function(state) {
+//   if (state === 'poweredOn') {
+//     noble.startScanning();
+//   } else {
+//     noble.stopScanning();
+//   }
+// });
+//
+//
+// bleno.on('stateChange', function(state) {
+// 	var advertisementData = new Buffer("I AM THE TEST DATA");
+//   if (state === 'poweredOn') {
+//     console.log('broadcasting on')
+//     bleno.startAdvertisingWithEIRData(advertisementData);
+//   }
+// });
 
-noble.on('stateChange', function(state) {
-  if (state === 'poweredOn') {
-    noble.startScanning();
-  } else {
-    noble.stopScanning();
-  }
-});
 
+var Bleacon = require('bleacon');
+var uuid = 'e2c56db5dffb48d2b060d0f5a71096e0';
+var major = 0; // 0 - 65535
+var minor = 0; // 0 - 65535
+var measuredPower = -59; // -128 - 127 (measured RSSI at 1 meter)
 
-noble.on('discover', function(peripheral) {
-  console.log('found', peripheral);
-  // peripheral.connect(function(error) {
-  //   console.log('connected to peripheral: ' + peripheral.uuid);
-  //   peripheral.discoverAllServicesAndCharacteristics((error, services, characteristics)=>{
-	// 	console.log('discovered', services, 'with characteristics', characteristics);
-	// });
-  // });
-});
+Bleacon.startAdvertising(uuid, major, minor, measuredPower);
 
-noble.on('stateChange', function(state) {
-  if (state === 'poweredOn') {
-    noble.startScanning();
-  } else {
-    noble.stopScanning();
-  }
-});
-
-
-bleno.on('stateChange', function(state) {
-	var advertisementData = new Buffer("I AM THE TEST DATA");
-  if (state === 'poweredOn') {
-    bleno.startAdvertisingWithEIRData(advertisementData);
-  }
+Bleacon.on('discover', function(bleacon) {
+  console.log('discovered', bleacon);
 });
